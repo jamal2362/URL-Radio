f8adf23a162bc8d9b2d97ec84f870a78f41b70bd
y20k <y20k@users.noreply.github.com>
2023-02-24 19:20:09 +0100

    fixes a crash that occurred, when the Next button on the soft keyboard was tapped

--- app/src/main/java/com/jamal2367/urlradio/collection/CollectionAdapter.kt
+++ app/src/main/java/com/jamal2367/urlradio/collection/CollectionAdapter.kt
@@ -22,6 +22,7 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -147,7 +148,13 @@ class CollectionAdapter(private val context: Context, private val collectionAdap
                         stationViewHolder.playButtonView.isGone = true
                         stationViewHolder.stationStarredView.isGone = true
                         stationViewHolder.editViews.isVisible = true
-                        stationViewHolder.stationUriEditView.isGone = !editStationStreamsEnabled
+                        if (editStationStreamsEnabled) {
+                            stationViewHolder.stationUriEditView.isVisible = true
+                            stationViewHolder.stationUriEditView.imeOptions = EditorInfo.IME_ACTION_DONE
+                        } else {
+                            stationViewHolder.stationUriEditView.isGone = true
+                            stationViewHolder.stationNameEditView.imeOptions = EditorInfo.IME_ACTION_DONE
+                        }
                     }
                     // hide edit views
                     else -> {
