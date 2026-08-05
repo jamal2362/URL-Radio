2112d5fb158a90f2eba04c7fa6e2216afaccec62
U3knOwn <jamal.2362@googlemail.com>
2026-08-05 11:15:01 +0200

    Bump version to 13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jamal2367.urlradio"
         minSdk = 23
         targetSdk = 37
-        versionCode = 128
-        versionName = "12.8"
+        versionCode = 130
+        versionName = "13.0"
     }
 
     buildFeatures {
